f80be9ddaa73ce1cca049096c94a81936855eb94
Sukij Sunitsakul <sukij44800@gmail.com>
2023-02-02 11:12:37 +0700

    fix minor bugs

--- routes/hospitals.js
+++ routes/hospitals.js
@@ -1,33 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
-const app = express()
-
 router.get('/', (req, res) => {
-  res.status(200).json({ success: true, data: { id: 1 } })
-})
-
-router.get('/api/v1/hospitals', (req, res) => {
   res.status(200).json({ success: true, msg: 'Show all hospitals' })
 })
 
-router.get('/api/v1/hospitals/:id', (req, res) => {
+router.get('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Show hospitals ${res.params.id}` })
 })
 
-router.post('/api/v1/hospitals', (req, res) => {
+router.post('/', (req, res) => {
   res.status(200).json({ success: true, msg: 'Create new hospitals' })
 })
 
-router.put('/api/v1/hospitals:id', (req, res) => {
+router.put('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Update hospital ${req.params.id}` })
 })
 
-router.delete('/api/v1/hospitals/:id', (req, res) => {
+router.delete('/:id', (req, res) => {
   res
     .status(200)
     .json({ success: true, msg: `Delete hospital ${res.params.id}` })
