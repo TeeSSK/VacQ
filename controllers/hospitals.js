068ab4089ccab7685ca417ef1b73dde225dd2206
Sukij Sunitsakul <sukij44800@gmail.com>
2023-02-09 11:06:51 +0700

    sync GET hospital(s) to db

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -3,17 +3,41 @@ const Hospital = require('../models/Hospital')
 // @desc Get all hospitals
 // @route GET /api/v1/hospitals
 // @access Public
-exports.getHospitals = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Show all hospitals' })
+exports.getHospitals = async (req, res, next) => {
+  try {
+    const hospitals = await Hospital.find()
+    res.status(200).json({
+      success: true,
+      msg: 'Show all hospitals',
+      count: hospitals.length,
+      data: hospitals,
+    })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc Get single hospital
 // @route GET /api/v1/hospitals/:id
 // @access Public
-exports.getHospital = (req, res, next) => {
-  res
-    .status(200)
-    .json({ success: true, msg: `Show hospitals ${req.params.id}` })
+exports.getHospital = async (req, res, next) => {
+  try {
+    const hospital = await Hospital.findById(req.params.id)
+
+    if (!hospital) {
+      return res.status(400).json({ success: false })
+    }
+
+    res
+      .status(200)
+      .json({
+        success: true,
+        msg: `Show hospitals ${req.params.id}`,
+        data: hospital,
+      })
+  } catch (err) {
+    res.status(400).json({ success: false })
+  }
 }
 
 // @desc create new hospital
