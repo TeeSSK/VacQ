f0b20de4cda2de1c4bafb61612cb81232ba3b93b
Sukij Sunitsakul <sukij44800@gmail.com>
2023-02-23 13:19:51 +0700

    create protect for check token and endpoint me

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,3 +77,11 @@ exports.login = async (req, res, next) => {
   // res.status(200).json({ success: true, token })
   sendTokenResponse(user, 200, res)
 }
+
+// @desc Get current Logged in user
+// @route POST /api/v1/auth/me
+// @access Private
+exports.getMe = async (req, res, next) => {
+  const user = await User.findById(req.user.id)
+  res.status(200).json({ success: true, data: user })
+}
