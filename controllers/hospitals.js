ec8e2c5aa8acb80c1502ade1409ab274803cddda
Sukij Sunitsakul <sukij44800@gmail.com>
2023-03-16 00:26:08 +0700

    add select and sort by query in getHospitals

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -5,7 +5,46 @@ const Hospital = require('../models/Hospital')
 // @access Public
 exports.getHospitals = async (req, res, next) => {
   try {
-    const hospitals = await Hospital.find()
+    let query
+
+    // Copy req.query
+    const reqQuery = { ...req.query }
+
+    // Fields to exclude
+    const removeFields = ['select', 'sort']
+
+    // Loop over removeFields and delete them from reqQuery
+    removeFields.forEach((param) => delete reqQuery[param])
+    console.log(reqQuery)
+
+    // Create query string
+    let queryStr = JSON.stringify(reqQuery)
+    // let queryStr = JSON.stringify(req.query)
+
+    // Create operators ($gt, $gte, etc)
+    queryStr = queryStr.replace(
+      /\b(gt|gte|lt|lte|in)\b/g,
+      (match) => `$${match}`
+    )
+
+    // Finding resource
+    query = Hospital.find(JSON.parse(queryStr))
+
+    console.log(req.query)
+    // Select Fields
+    if (req.query.select) {
+      const fields = req.query.select.split(',').join(' ')
+      query = query.select(fields)
+    }
+    // Sort
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ')
+      query = query.sort(sortBy)
+    } else {
+      query = query.sort('-createdAt')
+    }
+
+    const hospitals = await query
     res.status(200).json({
       success: true,
       msg: 'Show all hospitals',
